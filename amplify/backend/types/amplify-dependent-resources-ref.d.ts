ff1cf5ef4fa09601a118571d14b30c6a0af350a0
adamhinton <adam.d.hinton@gmail.com>
2023-03-26 13:26:11 -0400

    Initiate basic API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "poke151BEmar26": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "pokemon151projectfe9535b112": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- index.ts
+++ index.ts
@@ -20,11 +20,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+	res.send("Express + TypeScript Server");
 });
 
 app.use("/api", routes);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+export default app;
