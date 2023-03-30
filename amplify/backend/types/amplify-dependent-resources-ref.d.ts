8fe9499fbf56b4fecb1fcda191a40824896d3ca7
adamhinton <adam.d.hinton@gmail.com>
2023-03-29 20:24:55 -0400

    Make ampllfy API and put db, migrations and seeds in the function/src folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "poke151Mar292023": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "pokemon151projectfe756963cd": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
